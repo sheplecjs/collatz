c7ee6adecb86738bccfcc041d0216308e0902f6a
sheplecjs <colin.shepley@pm.me>
2023-05-07 19:15:50 +0100

    removed dead update history code

--- src/main.rs
+++ src/main.rs
@@ -184,47 +184,6 @@ fn collatz(n: BigInt) -> BigInt {
     }
 }
 
-// fn update_flat_file(n: BigInt, step: u32) {
-
-//     let df_old: DataFrame = read_history();
-//     let new_data: DataFrame = make_new_frame(n, step);
-//     let mut df_new: DataFrame = stack_old_and_new(df_old, new_data);
-//     df_new = get_unique_df(df_new);
-
-//     write_new_csv(df_new);
-
-//     fn write_new_csv(mut df: DataFrame) {
-//         let mut file = std::fs::File::create("history.csv").unwrap();
-//         CsvWriter::new(&mut file).finish(&mut df).unwrap();
-//     }
-    
-//     fn get_unique_df(df: DataFrame) -> DataFrame {
-//         df.unique(None, First).expect("DataFrame")
-//     }
-    
-//     fn stack_old_and_new(df_old: DataFrame, df_new: DataFrame) -> DataFrame {
-//         df_old.vstack(&df_new).unwrap()
-//     }
-    
-//     fn make_new_frame(n: BigInt, step: u32) -> DataFrame {
-//         df!("Number" => &[n.to_string()], "Steps" => &[step.to_string()]).expect("DataFrame")
-//     }
-    
-//     fn read_history() -> DataFrame {
-    
-//         if Path::new("history.csv").is_file() == false {
-//             let mut file: File = File::create("history.csv").expect("History file creation error");
-//             write!(file, "Number,Steps").expect("Issue writing headers to new history file.");
-//         }
-        
-//         CsvReader::from_path("history.csv")
-//                 .expect("File")
-//                 .has_header(true)
-//                 .infer_schema(Some(0)) // everything as utf8 so that BigInt cast will work
-//                 .finish().expect("DataFrame")
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use num_bigint::BigInt;
