16e6719412997cc7d9d007284a816d06fb32519b
sheplecjs <colin.shepley@pm.me>
2023-05-08 06:37:25 +0100

    cargo fmt

--- src/db.rs
+++ src/db.rs
@@ -1,35 +1,39 @@
 use num_bigint::BigInt;
-use polars::prelude::{DataFrame, CsvReader, CsvWriter, SerReader, SerWriter, NamedFrom};
-use polars::frame::UniqueKeepStrategy::First;
 use polars::df;
+use polars::frame::UniqueKeepStrategy::First;
+use polars::prelude::{CsvReader, CsvWriter, DataFrame, NamedFrom, SerReader, SerWriter};
+use postgres::{Client, NoTls};
+use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use std::fs::File;
-use postgres::{Client, NoTls};
 
 pub fn create_postgres_table(conn_string: String) {
     let mut client = Client::connect(&conn_string, NoTls).expect("Error establishing connection.");
-    client.execute("
+    client
+        .execute(
+            "
     CREATE TABLE IF NOT EXISTS collatz (
         id       SERIAL PRIMARY KEY,
         Number   TEXT NOT NULL,
         Steps    TEXT NOT NULL
     )
-    ").expect("Error creating psql table.");
-
+    ",
+        )
+        .expect("Error creating psql table.");
 }
 
 pub fn update_psql(n: BigInt, steps: u32, conn_string: String) {
     let mut client = Client::connect(&conn_string, NoTls).expect("Error establishing connection.");
 
-    client.execute(
-        "INSERT INTO collatz (Number, Steps) VALUES ($1, $2)",
-        &[&n.to_string(), &steps.to_string()],
-    ).expect("Error updating psql table.");
+    client
+        .execute(
+            "INSERT INTO collatz (Number, Steps) VALUES ($1, $2)",
+            &[&n.to_string(), &steps.to_string()],
+        )
+        .expect("Error updating psql table.");
 }
 
 pub fn update_flat_file(n: BigInt, step: u32) {
-
     let df_old: DataFrame = read_history();
     let new_data: DataFrame = make_new_frame(n, step);
     let mut df_new: DataFrame = stack_old_and_new(df_old, new_data);
@@ -41,30 +45,30 @@ pub fn update_flat_file(n: BigInt, step: u32) {
         let mut file = std::fs::File::create("history.csv").unwrap();
         CsvWriter::new(&mut file).finish(&mut df).unwrap();
     }
-    
+
     fn get_unique_df(df: DataFrame) -> DataFrame {
         df.unique(None, First).expect("DataFrame")
     }
-    
+
     fn stack_old_and_new(df_old: DataFrame, df_new: DataFrame) -> DataFrame {
         df_old.vstack(&df_new).unwrap()
     }
-    
+
     fn make_new_frame(n: BigInt, step: u32) -> DataFrame {
         df!("Number" => &[n.to_string()], "Steps" => &[step.to_string()]).expect("DataFrame")
     }
-    
+
     fn read_history() -> DataFrame {
-    
         if Path::new("history.csv").is_file() == false {
             let mut file: File = File::create("history.csv").expect("History file creation error");
             write!(file, "Number,Steps").expect("Issue writing headers to new history file.");
         }
-        
+
         CsvReader::from_path("history.csv")
-                .expect("File")
-                .has_header(true)
-                .infer_schema(Some(0)) // everything as utf8 so that BigInt cast will work
-                .finish().expect("DataFrame")
+            .expect("File")
+            .has_header(true)
+            .infer_schema(Some(0)) // everything as utf8 so that BigInt cast will work
+            .finish()
+            .expect("DataFrame")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::io;
-use std::time::Instant;
 use num_bigint::{BigInt, RandBigInt};
 use num_integer::Integer;
 use num_traits::sign::Signed;
+use std::io;
+use std::time::Instant;
 
 // history update funcs
 mod db;
 
 fn main() {
-
     let mut save_option = String::new();
 
     loop {
@@ -16,7 +15,7 @@ fn main() {
             "Input psql connection string to use db (default, postgresql://postgres:@db:5432/postgres).\n",
             "Input 'flat' to use flat file."
         ));
-    
+
         io::stdin()
             .read_line(&mut save_option)
             .expect("Failed to read line");
@@ -24,25 +23,23 @@ fn main() {
         match save_option.trim() {
             "flat" => {
                 break;
-            },
+            }
             "" => {
                 save_option = String::from("postgresql://postgres:@db:5432/postgres");
-            },
-            _ => {
-                
             }
+            _ => {}
         };
         db::create_postgres_table(save_option.clone());
         break;
-    };
+    }
 
     loop {
-        
         println!(concat!(
             "Input a positive integer to show its Collatz sequence.\n",
             "Input 'random' to use a random integer.\n",
             "Input 'range' to test a range of integers.\n",
-            "Input 'exit' to exit."));
+            "Input 'exit' to exit."
+        ));
 
         let mut collatz = String::new();
         let mut def_verbose = true;
@@ -52,50 +49,51 @@ fn main() {
             .expect("Failed to read line");
 
         let now = Instant::now();
-        
+
         let input: BigInt = match collatz.trim().parse() {
             Ok(big_int) => big_int, // specific number specified
             Err(_) => {
                 match collatz.trim() {
-                    "random" => { // random bits generator
+                    "random" => {
+                        // random bits generator
                         let d: BigInt;
                         def_verbose = false;
                         loop {
-                        println!("How many bits?");
-                        let mut digits = String::new();
-                        io::stdin()
-                            .read_line(&mut digits)
-                            .expect("Failed to read line");
-                        match digits.trim().parse() {
-                            Ok(num) => {
-                                let mut rng = rand::thread_rng();
-                                d = rng.gen_bigint(num).abs();
-                                break;
-                            }
-                            Err(_) => {
-                                println!("Input a valid number of bits.");
-                                continue;
-                            },
-                        };
-                    };
-                    d
-                    },
-                    "exit" => { // exit condition
+                            println!("How many bits?");
+                            let mut digits = String::new();
+                            io::stdin()
+                                .read_line(&mut digits)
+                                .expect("Failed to read line");
+                            match digits.trim().parse() {
+                                Ok(num) => {
+                                    let mut rng = rand::thread_rng();
+                                    d = rng.gen_bigint(num).abs();
+                                    break;
+                                }
+                                Err(_) => {
+                                    println!("Input a valid number of bits.");
+                                    continue;
+                                }
+                            };
+                        }
+                        d
+                    }
+                    "exit" => {
+                        // exit condition
                         break;
-                    },
-                    "range" => { // range condition
+                    }
+                    "range" => {
+                        // range condition
                         let mut start: BigInt;
                         let end: usize;
                         loop {
                             println!("Start:");
                             let mut s = String::new();
-                            io::stdin()
-                                .read_line(&mut s)
-                                .expect("Failed to read line");
+                            io::stdin().read_line(&mut s).expect("Failed to read line");
                             match s.trim().parse() {
                                 Ok(big_int) => {
                                     start = big_int;
-                                },
+                                }
                                 Err(_) => {
                                     println!("Invalid input.");
                                     continue;
@@ -103,14 +101,12 @@ fn main() {
                             }
                             println!("How many?");
                             let mut e = String::new();
-                            io::stdin()
-                                .read_line(&mut e)
-                                .expect("Failed to read line");
+                            io::stdin().read_line(&mut e).expect("Failed to read line");
                             match e.trim().parse() {
                                 Ok(num) => {
                                     end = num;
                                     break;
-                                },
+                                }
                                 Err(_) => {
                                     println!("Invalid input.");
                                     continue;
@@ -123,12 +119,13 @@ fn main() {
                             sequence(t, false, now_range, save_option.clone());
                         }
                         continue;
-                    },
-                    _ => { // non-recognized command
+                    }
+                    _ => {
+                        // non-recognized command
                         continue;
-                    },
+                    }
                 }
-            },
+            }
         };
 
         sequence(input, def_verbose, now, save_option.clone())
@@ -140,16 +137,15 @@ fn sequence(n: BigInt, verbose: bool, now: Instant, save_option: String) {
     let mut step: u32 = 1;
 
     loop {
-
         if verbose {
             let mut transform = String::from("(Base)");
             if seq != BigInt::from(1) {
-            let odd_transform: bool = seq.is_odd();
-            transform = String::from("(Reduce)");
-            if odd_transform {
-                transform = String::from("(Augment)");
+                let odd_transform: bool = seq.is_odd();
+                transform = String::from("(Reduce)");
+                if odd_transform {
+                    transform = String::from("(Augment)");
+                }
             }
-        }
             println!("Step {step}: {seq} - {transform}");
         }
 
@@ -161,11 +157,11 @@ fn sequence(n: BigInt, verbose: bool, now: Instant, save_option: String) {
             match save_option.trim() {
                 "flat" => {
                     db::update_flat_file(n, step);
-                },
-                
+                }
+
                 _ => {
                     db::update_psql(n, step, save_option.clone());
-                },
+                }
             };
 
             break;
@@ -178,7 +174,7 @@ fn sequence(n: BigInt, verbose: bool, now: Instant, save_option: String) {
 
 fn collatz(n: BigInt) -> BigInt {
     if n.is_odd() {
-        (n*3) + 1
+        (n * 3) + 1
     } else {
         n / 2
     }
@@ -186,9 +182,9 @@ fn collatz(n: BigInt) -> BigInt {
 
 #[cfg(test)]
 mod tests {
-    use num_bigint::BigInt;
     use super::*;
-    
+    use num_bigint::BigInt;
+
     #[test]
     fn test_sequence_odd() {
         let result_odd: BigInt = collatz(BigInt::from(7));
